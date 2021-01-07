2b8268b324ecefd1d8b4c9e53463b82f5ddd73b8
Luis Acedo Herrera <la.herrera457@gmail.com>
2021-01-07 17:21:37 +0100

    home ready for desktop version

--- pages/index.js
+++ pages/index.js
@@ -32,28 +32,30 @@ export default function Home() {
             <h1 className={styles.title_style}>E32</h1>
             <p className={styles.intro_text}>E32 is a trans-disciplinary studio that strives to enhance integrated projects with a "Human centric" approach.</p>
           </div>
-          <div className={styles.image_container}>
-            <Image src={currentBlock.image} width={1920} height={2670} layout="responsive" />
+          <div className={currentBlock.title === "L" ? styles.image_container__lighting_variant : styles.image_container}>
+            <Image className={currentBlock.title === "L" ? styles.image__lighting_variant : (currentBlock.title === "B" ? styles.image__building_variant : "")} src={currentBlock.image} width={currentBlock.title === "L" ? 2670 : 1920} height={currentBlock.title === "L" ? 1920 : 2670} layout="responsive" />
           </div>
           <div className={styles.address_info_container}>
             <p>Nº02 C/Energía, 32 Planta 1</p>
             <p>08940 Cornellà de Llobregat</p>
             <p>Barcelona</p>
           </div>
-          <div>
+          <div className={styles.block_sign_container}>
             <h2 className={currentBlock.title === "L" ? `${styles.title_style} ${styles.title_style_lighting}`: styles.title_style}>{currentBlock.title}</h2>
           </div>
       </div>
       <div className={styles.footer_home}>
-        <div>
+        <div className={styles.footer_home__address_container}>
           <p>Nº02 C/Energía, 32 Planta 1</p>
           <p>08940 Cornellà de Llobregat</p>
           <p>Barcelona</p>
         </div>
         <p>About</p>
-        <p onMouseLeave={()=>setBlock("architecture")} onMouseEnter={()=>setBlock("architecture")}>Architecture</p>
-        <p onMouseLeave={()=>setBlock("architecture")} onMouseEnter={()=>setBlock("building")}>Building</p>
-        <p onMouseLeave={()=>setBlock("architecture")} onMouseEnter={()=>setBlock("lighting")}>Lighting</p>
+        <div className={styles.footer_home__navbar}>
+          <p onMouseLeave={()=>setBlock("architecture")} onMouseEnter={()=>setBlock("architecture")}>Architecture</p>
+          <p onMouseLeave={()=>setBlock("architecture")} onMouseEnter={()=>setBlock("lighting")}>Lighting</p>
+          <p onMouseLeave={()=>setBlock("architecture")} onMouseEnter={()=>setBlock("building")}>Building</p>
+        </div>
       </div>
     </>
   )
