4af8ba43258cece1df07eaf77da45709dbe7221c
Luis Acedo Herrera <la.herrera457@gmail.com>
2021-03-02 23:31:59 +0100

    adding lock screen

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,9 @@ import AppLayout from "../components/AppLayout"
 import '../styles/global.scss'
 
 export default function App({ Component, pageProps }) {
-  // useEffect(()=>{
-  //   window.screen.orientation.lock('portrait')
-  // })
+  useEffect(()=>{
+    window.screen.orientation.lock('portrait')
+  })
 
   return (
     <AppLayout>
