c5388e5f4f81241a950ec0296816e5b02eca83e5
Luis Acedo Herrera <la.herrera457@gmail.com>
2021-03-02 23:21:40 +0100

    test screen orientation lock

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { useEffect } from "react"
 import AppLayout from "../components/AppLayout"
 import '../styles/global.scss'
 
 export default function App({ Component, pageProps }) {
+  useEffect(()=>{
+    window.screen.orientation.lock('portrait')
+  })
+
   return (
     <AppLayout>
       <Component {...pageProps} />
