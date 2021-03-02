ff58113b66c54efe250b34f6d81f1f1dee34d4f1
Luis Acedo Herrera <la.herrera457@gmail.com>
2021-03-02 23:47:01 +0100

    first iteration at screen lock

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,6 @@ import AppLayout from "../components/AppLayout"
 import '../styles/global.scss'
 
 export default function App({ Component, pageProps }) {
-  useEffect(()=>{
-    window.screen.orientation.lock('portrait')
-  })
 
   return (
     <AppLayout>
