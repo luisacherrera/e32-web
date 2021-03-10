e0b4ae1a160e9b2ffe8c910fe3c560ba2c973b8a
Luis Acedo Herrera <la.herrera457@gmail.com>
2021-03-10 15:09:14 +0100

    refactor lighting module

--- pages/lighting/index.js
+++ pages/lighting/index.js
@@ -1,54 +1,52 @@
-import { useRouter } from 'next/router'
-import styles from './Lighting.module.scss'
+import CarouselPage from '../../components/CarouselPage'
 
 export default function Lighting() {
-  const router = useRouter();
-
+  const data = [
+    {
+      title: 'Paseo de Gracia. Residential Apartments',
+      year: '2015',
+      location: 'Barcelona',
+      expedient: '123ABC',
+      imageURL: '/photos/lighting/Destacada_1.png',
+      isLandscape: true
+    },
+    {
+      title: 'Paseo de Gracia. Residential Apartments 2',
+      year: '2015',
+      location: 'Barcelona',
+      expedient: '123ABC',
+      imageURL: '/photos/lighting/2.jpg',
+      isLandscape: false
+    },
+    {
+      title: 'Paseo de Gracia. Residential Apartments 3',
+      year: '2015',
+      location: 'Barcelona',
+      expedient: '123ABC',
+      imageURL: '/photos/lighting/3.jpg',
+      isLandscape: true
+    },
+    {
+      title: 'Paseo de Gracia. Residential Apartments 4',
+      year: '2015',
+      location: 'Barcelona',
+      expedient: '123ABC',
+      imageURL: '/photos/lighting/4.jpg',
+      isLandscape: false
+    },
+    {
+      title: 'Paseo de Gracia. Residential Apartments 5',
+      year: '2015',
+      location: 'Barcelona',
+      expedient: '123ABC',
+      imageURL: '/photos/lighting/Destacada_2.png',
+      isLandscape: true
+    }
+  ]
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.header_logo}>
-          <h1 className={styles.title_style} onClick={()=>router.push('/')}>E32</h1>
-        </div>
-        <div className={styles.address_info_container}>
-          <address>
-            Nº02 C/Energía, 32 Planta 1<br/>
-            08940 Cornellà de Llobregat<br/>
-            Barcelona
-          </address>
-        </div>
-        <div className={styles.header_logo__page_variant}>
-          <h1 className={styles.title_style}>L</h1>
-        </div>
-        <div className={styles.footer}>
-          <div className={styles.footer__address_container}>
-            <h3>Paseo de Gracia</h3>
-            <h3>residential Apartments</h3>
-          </div>
-          <div className={styles.footer__info_container}>
-            <h3>Year: July 2015</h3>
-            <h3>Location: Barcelona</h3>
-            <h3>NºEXP: 123ABC</h3>
-          </div>
-          <p className={styles.footer_middle} onClick={()=>router.push('/about')}>About</p>
-          <ul className={styles.footer_home__navbar}>
-            <li onClick={()=>router.push('/architecture')} className={styles.footer_home__navbar_item_noactive}>Architecture</li>
-            <li className={styles.footer_home__navbar_item}>Lighting</li>
-            <li onClick={()=>router.push('/building')} className={styles.footer_home__navbar_item_noactive}>Building</li>
-          </ul>
-        </div>
-        <div className={styles.horizontal_container}>
-          <div className={styles.first_image_container}><img className={`${styles.image_item} ${styles.image_item__horizontal_variant} ${styles.image_item__lighting_variant}`} src="/photos/lighting/Destacada_1.png" /></div>
-          <div className={styles.image_item__double_spaced}><img className={`${styles.image_item} ${styles.image_item__vertical_variant}`} src="/photos/lighting/2.jpg" /></div>
-          <div><img className={`${styles.image_item} ${styles.image_item__horizontal_variant}`} src="/photos/lighting/3.jpg" /></div>
-          <div className={styles.image_item__single_spaced}><img className={`${styles.image_item} ${styles.image_item__vertical_variant}`} src="/photos/lighting/4.jpg" /></div>
-          <div><img className={`${styles.image_item} ${styles.image_item__horizontal_variant} ${styles.image_item__lighting_variant}`} src="/photos/lighting/Destacada_2.png" /></div>
-        </div>
-      </div>
-      <div className={styles.footer_about__mobile} onClick={()=>router.push('/about')}>
-        <h2 className={styles.footer_about__mobile_claim}>See more</h2>
-        <img onClick={()=>router.push('/about')} src="/cursor/SeeMore.png"/>
-      </div>
+      <CarouselPage carousel_data={data}
+                    category="lighting"></CarouselPage>
     </>
   )
 }
