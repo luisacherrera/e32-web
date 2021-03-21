e1c8523ffc3c81a72491ff2e3ab223de8b3d8ebe
Luis Acedo Herrera <la.herrera457@gmail.com>
2021-03-21 18:32:13 +0100

    add building projects

---
+++ pages/building/projects/index.js
@@ -0,0 +1,50 @@
+import ProjectPage from '../../../components/ProjectPage'
+import { getBuildingData } from '../../../lib/api'
+
+export default function BuildingProjects({data}) {
+  const project_data = [];
+
+  data.forEach((item)=>{
+    const customFields = item.acf
+
+    const items = Object.keys(customFields).reduce((acc,key)=>{
+      if (customFields[key].image && customFields[key].image !== false) {
+        acc.push({
+          imageId: customFields[key].image.ID, 
+          title: customFields[key].title,
+          year: customFields[key].year,
+          location: customFields[key].location,
+          expedient: customFields[key].expedient_number,
+          image: customFields[key].image?.url,
+          isLandscape: customFields[key].image?.width > customFields[key].image?.height ? true : false,
+          text: customFields[key].text && customFields[key].text
+        })
+      }
+  
+      return acc
+    },[])
+
+    const projectInfo = {
+      projectId: customFields.project_id,
+      projectDescription: customFields.project_description,
+      data: items
+    }
+
+    project_data.push(projectInfo)
+  })
+
+  return (
+    <ProjectPage project_items={project_data.reverse()}
+                 category={'building'}></ProjectPage>
+  )
+}
+
+export async function getServerSideProps() {
+  const data = await getBuildingData()
+
+  return {
+    props: {
+      data: data
+    }
+  }
+}
