85b3195f71750f26caaee3fc6627bc9fae8bd722
Luis Acedo Herrera <la.herrera457@gmail.com>
2021-03-23 21:33:37 +0100

    lighting uses statics

--- pages/lighting/projects/index.js
+++ pages/lighting/projects/index.js
@@ -1,50 +1,9 @@
 import ProjectPage from '../../../components/ProjectPage'
-import { getLightingData } from '../../../lib/api'
+import lighting_projects from '../../../lib/lighting_projects'
 
-export default function LightingProjects({data}) {
-  const project_data = [];
-
-  data.forEach((item)=>{
-    const customFields = item.acf
-
-    const items = Object.keys(customFields).reduce((acc,key)=>{
-      if (customFields[key].image && customFields[key].image !== false) {
-        acc.push({
-          imageId: customFields[key].image.ID, 
-          title: customFields[key].title,
-          year: customFields[key].year,
-          location: customFields[key].location,
-          expedient: customFields[key].expedient_number,
-          image: customFields[key].image?.url,
-          isLandscape: customFields[key].image?.width > customFields[key].image?.height ? true : false,
-          text: customFields[key].text && customFields[key].text
-        })
-      }
-  
-      return acc
-    },[])
-
-    const projectInfo = {
-      projectId: customFields.project_id,
-      projectDescription: customFields.project_description,
-      data: items
-    }
-
-    project_data.push(projectInfo)
-  })
-
+export default function LightingProjects() {
   return (
-    <ProjectPage project_items={project_data.reverse()}
+    <ProjectPage project_items={lighting_projects}
                  category={'lighting'}></ProjectPage>
   )
-}
-
-export async function getServerSideProps() {
-  const data = await getLightingData()
-
-  return {
-    props: {
-      data: data
-    }
-  }
 }
