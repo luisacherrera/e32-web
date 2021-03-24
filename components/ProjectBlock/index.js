28f6e5aff2a6b698eb7ba134db87366d48be61b4
Luis Acedo Herrera <la.herrera457@gmail.com>
2021-03-24 21:18:52 +0100

    adjust sizes counter numbers mobile

--- components/ProjectBlock/index.js
+++ components/ProjectBlock/index.js
@@ -50,7 +50,13 @@ export default function ProjectBlock({
       <div className={ isFirstElement ? ` ${styles.first_element} ${styles.project_intro_container}` : `${styles.project_intro_container}` }>
         <div className={styles.project_intro__counter}>
           <h2>Project</h2>
-          <h2>{ project_position < 10 ? `0${project_position}` : project_position }/{ total_project_length < 10 ? `0${total_project_length}` : total_project_length }</h2>
+          <h2>{ 
+            project_position < 10 
+              ? `0${project_position}` 
+              : project_position }/{ total_project_length < 10 
+                ? `0${total_project_length}` 
+                : total_project_length 
+          }</h2>
           {
             project_description.length > 0 && <p>{ project_description }</p>
           }
