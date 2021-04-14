1086c4433e14ae4af2cad32e49f551788bd84a2d
Luis Acedo Herrera <la.herrera457@gmail.com>
2021-04-14 23:25:49 +0200

    remove background colors for body

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,6 @@ export default function Home() {
   const router = useRouter()
 
   useEffect(()=>{
-    document.body.style.background = '#fff'
     if (!isBrowser) {
       router.push('/architecture')
     }
